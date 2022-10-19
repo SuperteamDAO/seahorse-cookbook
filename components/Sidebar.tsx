8a5e3db43afdb5de87061765b89439a2fec11593
0xcatrovacer <swarnabgarang@gmail.com>
2022-10-19 23:40:22 +0530

    feat: Fixed and styled Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,12 +1,8 @@
 import React, { ReactNode } from "react";
 import {
-	IconButton,
-	Avatar,
 	Box,
 	CloseButton,
 	Flex,
-	HStack,
-	VStack,
 	Icon,
 	useColorModeValue,
 	Link,
@@ -16,35 +12,23 @@ import {
 	useDisclosure,
 	BoxProps,
 	FlexProps,
-	Menu,
-	MenuButton,
-	MenuDivider,
-	MenuItem,
-	MenuList,
+	IconButton,
 } from "@chakra-ui/react";
-import {
-	FiHome,
-	FiTrendingUp,
-	FiCompass,
-	FiStar,
-	FiSettings,
-	FiMenu,
-	FiBell,
-	FiChevronDown,
-} from "react-icons/fi";
+import { FiCode, FiBookOpen, FiPenTool, FiMenu } from "react-icons/fi";
 import { IconType } from "react-icons";
 import { ReactText } from "react";
+import CookbookLogo from "../public/CookBookLogo.png";
+import Image from "next/image";
 
 interface LinkItemProps {
 	name: string;
 	icon: IconType;
+	disabled: boolean;
 }
 const LinkItems: Array<LinkItemProps> = [
-	{ name: "Home", icon: FiHome },
-	{ name: "Trending", icon: FiTrendingUp },
-	{ name: "Explore", icon: FiCompass },
-	{ name: "Favourites", icon: FiStar },
-	{ name: "Settings", icon: FiSettings },
+	{ name: "Program Library", icon: FiBookOpen, disabled: false },
+	{ name: "CLI Tools", icon: FiCode, disabled: true },
+	{ name: "Tutorials", icon: FiPenTool, disabled: true },
 ];
 
 export default function Sidebar() {
@@ -62,7 +46,8 @@ export default function Sidebar() {
 				onClose={onClose}
 				returnFocusOnClose={false}
 				onOverlayClick={onClose}
-				size="full">
+				size="full"
+			>
 				<DrawerContent>
 					<SidebarContent onClose={onClose} />
 				</DrawerContent>
@@ -87,18 +72,38 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
 			bg={useColorModeValue("white", "gray.900")}
 			borderRight="1px"
 			borderRightColor={useColorModeValue("gray.200", "gray.700")}
-			w={{ base: "full", md: 60 }}
+			w={{ base: "full", md: 80 }}
 			pos="fixed"
 			h="full"
-			{...rest}>
-			<Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
-				<Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
-					Logo
+			{...rest}
+		>
+			<Flex
+				h="20"
+				alignItems="center"
+				mx="6"
+				justifyContent="space-between"
+			>
+				<Image
+					src={CookbookLogo}
+					height={30}
+					width={30}
+					style={{ borderRadius: 8, marginRight: 0 }}
+				/>
+				<Text fontSize="2xl" fontWeight="bold">
+					Seahorse Cookbook
 				</Text>
-				<CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
+				<CloseButton
+					display={{ base: "flex", md: "none" }}
+					onClick={onClose}
+				/>
 			</Flex>
 			{LinkItems.map((link) => (
-				<NavItem key={link.name} icon={link.icon}>
+				<NavItem
+					mt={4}
+					key={link.name}
+					icon={link.icon}
+					linkDisabled={link.disabled}
+				>
 					{link.name}
 				</NavItem>
 			))}
@@ -109,32 +114,44 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
 interface NavItemProps extends FlexProps {
 	icon: IconType;
 	children: ReactText;
+	linkDisabled: boolean;
 }
-const NavItem = ({ icon, children, ...rest }: NavItemProps) => {
+const NavItem = ({ icon, linkDisabled, children, ...rest }: NavItemProps) => {
 	return (
 		<Link
 			href="#"
 			style={{ textDecoration: "none" }}
-			_focus={{ boxShadow: "none" }}>
+			_focus={{ boxShadow: "none" }}
+		>
 			<Flex
 				align="center"
 				p="4"
-				mx="4"
+				mx="6"
 				borderRadius="lg"
 				role="group"
-				cursor="pointer"
-				_hover={{
-					bg: "cyan.400",
-					color: "white",
-				}}
-				{...rest}>
+				cursor={linkDisabled ? "not-allowed" : "pointer"}
+				_hover={
+					linkDisabled
+						? {
+								bg: "#4d586e",
+								color: "#b5b5b5",
+						  }
+						: {
+								bg: "#FB7185",
+								color: "#171923",
+						  }
+				}
+				{...rest}
+			>
 				{icon && (
 					<Icon
 						mr="4"
 						fontSize="16"
-						_groupHover={{
-							color: "white",
-						}}
+						_groupHover={
+							linkDisabled
+								? { color: "#b5b5b5" }
+								: { color: "#171923" }
+						}
 						as={icon}
 					/>
 				)}
@@ -154,11 +171,9 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
 			px={{ base: 4, md: 4 }}
 			height="20"
 			alignItems="center"
-			bg={useColorModeValue("white", "gray.900")}
-			borderBottomWidth="1px"
-			borderBottomColor={useColorModeValue("gray.200", "gray.700")}
 			justifyContent={{ base: "space-between", md: "flex-end" }}
-			{...rest}>
+			{...rest}
+		>
 			<IconButton
 				display={{ base: "flex", md: "none" }}
 				onClick={onOpen}
@@ -171,58 +186,15 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
 				display={{ base: "flex", md: "none" }}
 				fontSize="2xl"
 				fontFamily="monospace"
-				fontWeight="bold">
-				Logo
-			</Text>
-
-			<HStack spacing={{ base: "0", md: "6" }}>
-				<IconButton
-					size="lg"
-					variant="ghost"
-					aria-label="open menu"
-					icon={<FiBell />}
+				fontWeight="bold"
+			>
+				<Image
+					src={CookbookLogo}
+					height={40}
+					width={40}
+					style={{ borderRadius: 8 }}
 				/>
-				<Flex alignItems={"center"}>
-					<Menu>
-						<MenuButton
-							py={2}
-							transition="all 0.3s"
-							_focus={{ boxShadow: "none" }}>
-							<HStack>
-								<Avatar
-									size={"sm"}
-									src={
-										"https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
-									}
-								/>
-								<VStack
-									display={{ base: "none", md: "flex" }}
-									alignItems="flex-start"
-									spacing="1px"
-									ml="2">
-									<Text fontSize="sm">Justina Clark</Text>
-									<Text fontSize="xs" color="gray.600">
-										Admin
-									</Text>
-								</VStack>
-								<Box display={{ base: "none", md: "flex" }}>
-									<FiChevronDown />
-								</Box>
-							</HStack>
-						</MenuButton>
-						<MenuList
-							bg={useColorModeValue("white", "gray.900")}
-							borderColor={useColorModeValue("gray.200", "gray.700")}>
-							<MenuItem>Profile</MenuItem>
-							<MenuItem>Settings</MenuItem>
-							<MenuItem>Billing</MenuItem>
-							<MenuDivider />
-							<MenuItem>Sign out</MenuItem>
-						</MenuList>
-					</Menu>
-				</Flex>
-			</HStack>
+			</Text>
 		</Flex>
 	);
 };
-
